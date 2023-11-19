3646fc3f37d66dfce809f216ec9e30f723f14782
Dang Nguyen MInh Thien <thienchatdocmaudacam@gmail.com>
2023-11-19 09:53:31 +0700

    udate image

---
+++ input.py
@@ -0,0 +1,90 @@
+import pygame
+import numpy as np
+
+# Khởi tạo Pygame
+pygame.init()
+
+# Kích thước mỗi ô trên màn hình
+cell_size = 20
+
+# Kích thước ma trận
+rows, cols = 28, 28
+
+# Tạo một mảng 2 chiều để lưu trữ giá trị của mỗi ô
+grid = np.zeros((rows, cols))
+
+# Màu sắc
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+
+# Kích thước màn hình
+width, height = cols * cell_size, rows * cell_size + 50  # Thêm 50 đơn vị chiều cao cho phần nút
+
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Mouse Grid Input")
+
+# Tạo nút reset
+reset_button = pygame.Rect(0, rows * cell_size, width // 2, height - rows * cell_size)
+
+hello_font = pygame.font.SysFont(None, 30)
+hello_text = hello_font.render('Hello', True, (0, 0, 0))
+hello_text_rect = hello_text.get_rect(center=(width//2, height-70)) 
+
+font = pygame.font.Font(None, 36)
+
+# Tạo nút classify
+classify_button = pygame.Rect(width // 2, rows * cell_size, width // 2, height - rows * cell_size)
+
+running = True
+drawing = False  # Trạng thái nhấn giữ chuột
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:  # Kiểm tra nút chuột trái được nhấn
+                x, y = event.pos
+                if reset_button.collidepoint(x, y):
+                    
+                    grid = np.zeros((rows, cols))
+                elif classify_button.collidepoint(x, y):
+                    font = pygame.font.SysFont(None, 100)
+                    text = font.render("jdhas", True, (0, 255, 0))
+                    screen.blit(text, (10, height // 2)) 
+                else:
+                    drawing = True  # Bắt đầu nhấn giữ chuột để vẽ
+
+        if event.type == pygame.MOUSEBUTTONUP:
+            drawing = False  
+
+        if event.type == pygame.MOUSEMOTION and drawing:
+            x, y = event.pos
+            row, col = y // cell_size, x // cell_size
+            grid[row][col] = 1  
+
+    # Vẽ nút reset
+    pygame.draw.rect(screen, (200, 200, 200), reset_button)
+    reset_font = pygame.font.SysFont(None, 30)
+    reset_text = reset_font.render('Reset', True, (0, 0, 0))
+    screen.blit(reset_text, (10, rows * cell_size + 10))
+    screen.blit(hello_text, hello_text_rect)
+    # Vẽ nút classify
+    pygame.draw.rect(screen, (200, 200, 200), classify_button)
+    classify_font = pygame.font.SysFont(None, 30)
+    classify_text = classify_font.render('Classify', True, (0, 0, 0))
+    screen.blit(classify_text, (width // 2 + 10, rows * cell_size + 10))
+
+    # Vẽ lại màn hình dựa trên giá trị của mảng
+    for row in range(rows):
+        for col in range(cols):
+            color = BLACK if grid[row][col] == 0 else RED
+            pygame.draw.rect(screen, color, (col * cell_size, row * cell_size, cell_size, cell_size))
+
+    # Cập nhật màn hình
+    pygame.display.flip()
+
+# Kết thúc game
+pygame.quit()
---
+++ save_image.py
@@ -0,0 +1,74 @@
+import pygame
+import os
+from PIL import Image, ImageDraw
+
+WIDTH = 400
+HEIGHT = 400
+DRAW_COLOR = (255, 255, 255)  # Màu nét vẽ
+BG_COLOR = (0, 0, 0)  # Màu nền
+
+# Khởi tạo Pygame
+pygame.init()
+
+# Tạo cửa sổ
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Vẽ và Lưu Nét Vẽ")
+
+drawing = False
+last_pos = None
+radius = 8
+
+# Tạo ảnh trống để vẽ
+img = Image.new('RGB', (WIDTH, HEIGHT), BG_COLOR)
+draw = ImageDraw.Draw(img)
+
+# Vẽ nút reset
+font = pygame.font.Font(None, 36)
+reset_text = font.render('Reset', True, (255, 255, 255))
+reset_rect = reset_text.get_rect(center=(WIDTH // 2, HEIGHT - 50))
+image_count = 1 
+# Vòng lặp chính
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if reset_rect.collidepoint(event.pos):
+                # Xóa hình ảnh và màn hình
+                img = Image.new('RGB', (WIDTH, HEIGHT), BG_COLOR)
+                draw = ImageDraw.Draw(img)
+                screen.fill(BG_COLOR)
+            else:
+                drawing = True
+                pygame.draw.circle(screen, DRAW_COLOR, event.pos, radius)
+                if last_pos:
+                    draw.line([last_pos, event.pos], DRAW_COLOR, width=radius*2)
+                last_pos = event.pos
+        elif event.type == pygame.MOUSEMOTION and drawing:
+            pygame.draw.circle(screen, DRAW_COLOR, event.pos, radius)
+            if last_pos:
+                draw.line([last_pos, event.pos], DRAW_COLOR, width=radius*2)
+            last_pos = event.pos
+        elif event.type == pygame.MOUSEBUTTONUP:
+            drawing = False
+            last_pos = None
+
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_s and not drawing:
+            # Lưu nét vẽ vào hình ảnh 28x28
+            img_28x28 = img.resize((28, 28))
+            folder_path = "image"
+            if not os.path.exists(folder_path):
+                os.makedirs(folder_path)
+            file_name = f"alpha_{image_count}.png"  
+            file_path = os.path.join(folder_path, file_name)  
+            img_28x28.save(file_path)
+            image_count += 1
+
+    # Vẽ nút reset lên màn hình
+    screen.blit(reset_text, reset_rect)
+
+    pygame.display.flip()
+
+# Kết thúc Pygame
+pygame.quit()
