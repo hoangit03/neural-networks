7bd66bed74bd9c890a0e0d9eef417e69616a95a1
slayer <somkindofthat@gmail.com>
2023-11-20 12:43:54 +0700

    psi

--- save_image.py
+++ save_image.py
@@ -62,10 +62,10 @@ while running:
         if event.type == pygame.KEYDOWN and event.key == pygame.K_s and not drawing:
             # Lưu nét vẽ vào hình ảnh 28x28
             img_28x28 = img.resize((28, 28))
-            folder_path = "image/omega"
+            folder_path = "C:/Users/Duc Anh/Desktop/daihoc/jaba/New folder/neural-networks/image/psi"
             if not os.path.exists(folder_path):
                 os.makedirs(folder_path)
-            file_name = f"omega_{image_count}.png"  
+            file_name = f"psi_{image_count}.png"  
             # file_name = f"aplpha_192.png" 
             file_path = os.path.join(folder_path, file_name)  
             img_28x28.save(file_path)
