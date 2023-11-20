35a6fe87e2e11980c67add86f0a56cc45c869090
ldnam55 <fsfsnam7@gmail.com>
2023-11-20 11:17:57 +0700

    update img

--- save_image.py
+++ save_image.py
@@ -26,7 +26,7 @@ draw = ImageDraw.Draw(img)
 font = pygame.font.Font(None, 36)
 reset_text = font.render('Reset', True, (255, 255, 255))
 reset_rect = reset_text.get_rect(center=(WIDTH // 2, HEIGHT - 50))
-image_count = 501
+image_count = 571
 # Vòng lặp chính
 running = True
 while running:
@@ -62,10 +62,10 @@ while running:
         if event.type == pygame.KEYDOWN and event.key == pygame.K_s and not drawing:
             # Lưu nét vẽ vào hình ảnh 28x28
             img_28x28 = img.resize((28, 28))
-            folder_path = "image/omega"
+            folder_path = "image/eta"
             if not os.path.exists(folder_path):
                 os.makedirs(folder_path)
-            file_name = f"omega_{image_count}.png"  
+            file_name = f"eta_{image_count}.png"  
             # file_name = f"aplpha_192.png" 
             file_path = os.path.join(folder_path, file_name)  
             img_28x28.save(file_path)
