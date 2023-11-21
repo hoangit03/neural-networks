38428e0ca25bf01a83dec846360a0557139503a6
ldnam55 <Fsfsnam7@gmail.com>
2023-11-22 01:24:42 +0700

    alpha

--- save_image.py
+++ save_image.py
@@ -64,10 +64,10 @@ while running:
         if event.type == pygame.KEYDOWN and event.key == pygame.K_s and not drawing:
             # Lưu nét vẽ vào hình ảnh 28x28
             img_28x28 = img.resize((28, 28))
-            folder_path = "image/alpha"
+            folder_path = "image/beta"
             if not os.path.exists(folder_path):
                 os.makedirs(folder_path)
-            file_name = f"alpha_{image_count}.png"  
+            file_name = f"beta_{image_count}.png"  
             # file_name = f"aplpha_192.png" 
             file_path = os.path.join(folder_path, file_name)  
             img_28x28.save(file_path)
