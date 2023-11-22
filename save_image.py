c02bf55d5f7ff3b142ef5f337ac0c83d238e99b6
ldnam55 <Fsfsnam7@gmail.com>
2023-11-23 01:02:27 +0700

    datta

--- save_image.py
+++ save_image.py
@@ -28,7 +28,7 @@ reset_text = font.render('Reset', True, (255, 255, 255))
 reset_rect = reset_text.get_rect(center=(WIDTH // 2, HEIGHT - 50))
 
 
-image_count = 1
+image_count = 1062
 
 
 # Vòng lặp chính
@@ -68,10 +68,10 @@ while running:
             img_28x28 = img.resize((28, 28))
 
 
-            folder_path = "image/iota"
+            folder_path = "image/sigma"
             if not os.path.exists(folder_path):
                 os.makedirs(folder_path)
-            file_name = f"iota_{image_count}.png"  
+            file_name = f"sigma_{image_count}.png"  
 
 
             # file_name = f"aplpha_192.png" 
