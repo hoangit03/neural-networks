5a4bc89da5357a44f2e8ff6ef5d83f59ed1e39ce
ldnam55 <Fsfsnam7@gmail.com>
2023-11-23 10:33:53 +0700

    cuoi ky

--- neural_networks.py
+++ neural_networks.py
@@ -5,13 +5,13 @@ np.seterr(over='ignore')
 
 class NeuralNetwork:
     def __init__(self):
-        self.w1 = np.random.rand(784, 256) * np.sqrt(1/784)
+        self.w1 = np.random.randn(784, 256) * np.sqrt(1/784)
         self.b1 = np.zeros((1, 256))
 
-        self.w2 = np.random.rand(256, 128) * np.sqrt(1/256)
+        self.w2 = np.random.randn(256, 128) * np.sqrt(1/256)
         self.b2 = np.zeros((1, 128))
 
-        self.w_output = np.random.rand(128, 24) * np.sqrt(1/128)  
+        self.w_output = np.random.randn(128, 24) * np.sqrt(1/128)  
         self.b_output = np.zeros((1, 24))
 
     def _tanh(self, x):
@@ -33,24 +33,24 @@ class NeuralNetwork:
         return np.where(x > 0, 1, 0)
 
     def forward(self, input_data):
-        self.layer1 = self._tanh(np.dot(input_data , self.w1) + self.b1)
-        self.layer2 = self._tanh(np.dot(self.layer1 , self.w2) + self.b2)
-        self.output = self._sigmoid(np.dot(self.layer2 , self.w_output) + self.b_output)
+        self.layer1 = self._tanh(input_data @ self.w1 + self.b1)
+        self.layer2 = self._tanh(self.layer1 @ self.w2 + self.b2)
+        self.output = self._sigmoid(self.layer2 @ self.w_output + self.b_output)
         return self.output
 
     def backpropagate(self, input_data, error, learning_rate):
         output_delta = error * self._sigmoid_deriv(self.output)
 
-        layer2_error = np.dot(output_delta , self.w_output.T)
+        layer2_error = output_delta @ self.w_output.T
         layer2_delta = layer2_error * self._tanh_deriv(self.layer2)
-        layer1_error = np.dot(layer2_delta , self.w2.T)
+        layer1_error = layer2_delta @ self.w2.T
         layer1_delta = layer1_error * self._tanh_deriv(self.layer1)
 
-        self.w_output += np.dot(self.layer2.T , output_delta) * learning_rate
+        self.w_output += self.layer2.T @ output_delta * learning_rate
         self.b_output += np.sum(output_delta, axis=0, keepdims=True) * learning_rate
-        self.w2 += np.dot(self.layer1.T , layer2_delta) * learning_rate
+        self.w2 += self.layer1.T @ layer2_delta * learning_rate
         self.b2 += np.sum(layer2_delta, axis=0, keepdims=True) * learning_rate
-        self.w1 += np.dot(input_data.T , layer1_delta) * learning_rate
+        self.w1 += input_data.T @ layer1_delta * learning_rate
         self.b1 += np.sum(layer1_delta, axis=0, keepdims=True) * learning_rate
 
     def train(self, input_data, target_output, test_data, test_label, learning_rate=0.001, epochs=1000):
